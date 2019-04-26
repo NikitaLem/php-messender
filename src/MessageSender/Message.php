b08b1db7ea08d765c8f28f0b202191843fe962a6
Никита Лемешенко <saintsuigintoufag@gmail.com>
2019-04-26 17:53:31 +0600

    first commit

---
+++ src/MessageSender/Message.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace MessageSender;
+
+class Message {
+
+   public function method1($param1){
+			return "Hello World";
+   }
+}
---
+++ tests/MessageTest.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Message; 
+
+echo (new Message)->method1();
